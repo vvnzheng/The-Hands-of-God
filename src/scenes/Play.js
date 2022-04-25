817366c18e29b3a5b4dfa17a24971e75fa6c5128
amoua4 <103337992+amoua4@users.noreply.github.com>
2022-04-24 17:05:37 -0700

    implemented animations/art and change game feel

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -2,34 +2,56 @@ class VariableJump extends Phaser.Scene {
     constructor() {
         super('variableJumpScene');
     }
+
+    preload() {
+        this.load.spritesheet('walk', 'assets/pinkchild_walk.png', {frameWidth: 64, frameHeight: 128, startFrame: 0, endFrame: 7});
+    }
+    
     create() {
         // variables and settings
-        this.ACCELERATION = 1500;
+        this.ACCELERATION = 700;
         this.MAX_X_VEL = 500;   // pixels/second
         this.MAX_Y_VEL = 5000;
-        this.DRAG = 600;    // DRAG < ACCELERATION = icy slide
+        this.DRAG = 900;    // DRAG < ACCELERATION = icy slide
         this.MAX_JUMPS = 2; // change for double/triple/etc. jumps 🤾‍♀️
-        this.JUMP_VELOCITY = -700;
+        this.JUMP_VELOCITY = -500;
         currentScene = 3;
         this.physics.world.gravity.y = 2600;
 
-        // print Level name
-        this.add.text(game.config.width/2, 30, 'Level 1: Baby', { font: '20px Futura', fill: '#FFFFFF' }).setOrigin(0.5);
+        // background 
+        this.background = this.add.tileSprite(0,0, game.config.width, game.config.height, 'background').setOrigin(0,0);
 
-        // make ground tiles group
-        this.ground = this.add.group();
+        // ground
+        this.ground = this.physics.add.sprite(0, game.config.height - tileSize, 'ground').setOrigin(0);
+        this.ground.body.immovable = true;
+        this.ground.body.allowGravity = false;
+        
+        //this.ground = this.add.group();
+        /*
         for(let i = 0; i < game.config.width; i += tileSize) {
             let groundTile = this.physics.add.sprite(i, game.config.height - tileSize, 'platformer_atlas', 'block').setScale(SCALE).setOrigin(0);
             groundTile.body.immovable = true;
             groundTile.body.allowGravity = false;
             this.ground.add(groundTile);
         }
+        
         for(let i = tileSize*2; i < game.config.width-tileSize*13; i += tileSize) {
             let groundTile = this.physics.add.sprite(i, game.config.height - tileSize*9, 'platformer_atlas', 'block').setScale(SCALE).setOrigin(0);
             groundTile.body.immovable = true;
             groundTile.body.allowGravity = false;
             this.ground.add(groundTile);
         }
+        */
+
+        //animations
+        this.anims.create({
+            key: 'player_walk',
+            frames: this.anims.generateFrameNumbers('walk', {start: 0, end: 7, first: 0}),
+            frameRate:15,
+            repeat:0
+        })
+
+        
 
         //add god hands
         this.god01 = new God(this, game.config.width, 32, 'god');
@@ -52,7 +74,7 @@ class VariableJump extends Phaser.Scene {
         this.gameOver = false;
 
         // set up my alien son 👽
-        this.alien = this.physics.add.sprite(game.config.width/2, game.config.height/2, 'platformer_atlas', 'front').setScale(SCALE);
+        this.alien = this.physics.add.sprite(game.config.width/2, game.config.height/2, 'walk').setScale(SCALE);
         this.alien.setCollideWorldBounds(true);
         this.alien.setMaxVelocity(this.MAX_X_VEL, this.MAX_Y_VEL);
 
@@ -75,6 +97,9 @@ class VariableJump extends Phaser.Scene {
 
         // add physics collider
         this.physics.add.collider(this.alien, this.ground);
+
+        // print Level name
+        this.add.text(game.config.width/2, 30, 'Level 1: Baby', { font: '20px Futura', fill: '#000000' }).setOrigin(0.5);
     }
 
     update() {
@@ -84,12 +109,12 @@ class VariableJump extends Phaser.Scene {
             this.alien.setFlip(true, false);
             // see: https://photonstorm.github.io/phaser3-docs/Phaser.GameObjects.Components.Animation.html#play__anchor
             // play(key [, ignoreIfPlaying] [, startFrame])
-            //this.alien.anims.play('walk', true);
+            this.alien.anims.play('player_walk', true);
             this.leftKey.tint = 0xFACADE;   // tint key
         } else if(cursors.right.isDown) {
             this.alien.body.setAccelerationX(this.ACCELERATION);
             this.alien.resetFlip();
-            //this.alien.anims.play('walk', true);
+            this.alien.anims.play('player_walk', true);
             this.rightKey.tint = 0xFACADE;  // tint key
         } else {
             // set acceleration to 0 so DRAG will take over
